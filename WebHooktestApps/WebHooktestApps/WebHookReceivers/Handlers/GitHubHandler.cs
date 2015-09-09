60f87c7041cf15a44d0972425ecd78e78d6b859b
sorin.pochtar <sorin.pochtar@gmail.com>
2015-09-09 19:04:51 +0300

    1

---
+++ WebHooktestApps/WebHooktestApps/WebHookReceivers/Handlers/GitHubHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.WebHooks;
+using Newtonsoft.Json.Linq;
+
+namespace WebHookServer.WebHookReceivers.Handlers
+{
+    public class GitHubHandler : WebHookHandler
+    {
+        public override Task ExecuteAsync(string receiver, WebHookHandlerContext context)
+        {
+            JObject json = context.GetDataOrDefault<JObject>();
+
+            return Task.FromResult(true);
+        }
+    }
+}
